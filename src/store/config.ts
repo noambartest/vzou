31c71831939b9151c6941eebb59c9c6cc4596a45
Pavel <87754563+NiPavel@users.noreply.github.com>
2024-02-04 14:02:35 +0200

    Changed api requset address to a local in 'store/config.ts' file (#14)

--- src/store/config.ts
+++ src/store/config.ts
@@ -1,1 +1,4 @@
-export const API_URL = "https://ds-sce.sce-fpm.com:3001/api";
+// export const API_URL = "https://ds-sce.sce-fpm.com:3001/api";
+
+// its is a local path do backend, delete this in end of the proj and uncomment the string above
+export const API_URL = "http://localhost:3001/api";
