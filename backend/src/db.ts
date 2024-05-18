f58eb7a4f5162f498d57cd4cd352e39f95d1cdc5
alechkos <93551621+alechkos@users.noreply.github.com>
2024-05-18 20:52:33 +0300

    refactor: prepare for release

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,25 +3,11 @@ import { Sequelize } from 'sequelize'
 
 dotenv.config()
 
-// It is a production db connection, when we will finish this proj -> uncomment this
-// const DB_NAME = process.env.NODE_ENV === 'dev' ?process.env.TEST_DB_NAME!: process.env.DB_NAME!
-// export const sequelize = new Sequelize(
-//   DB_NAME,
-//   process.env.DB_USER!,
-//   process.env.DB_PASSWORD,
-//   {
-//     dialect: 'postgres',
-//     host: process.env.DB_HOST,
-//     port: 5432
-//   }
-// )
 
-// comment the section before the production
 export const sequelize = new Sequelize(
   'avds', //enter you local db name
   'postgres', // do no touch
   process.env.TEST_DB_PASSWORD, // enter your local db password
-  //do not touch this obj
   {
     dialect: 'postgres',
     host: 'localhost',
